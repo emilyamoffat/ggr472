ed00b4da19e31ba2eb44b7d69be36f7006c6b235
emilyamoffat <emilyamoffat@gmail.com>
2025-01-29 11:43:51 -0500

    Added files for GitHub Pages

---
+++ script.js
@@ -0,0 +1,7 @@
+// Reference the button element in the DOM
+const btn = document.getElementById("my-button");
+
+// Attach a "click" event listener to the button
+btn.addEventListener("click", () => {
+    alert("You clicked me!");
+});
